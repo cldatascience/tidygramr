864cd66ed305db871b7a673e8f3dc931857fba7f
Caoilfhionn Lane <cl.datascience@gmail.com>
2017-12-10 16:17:23 +0000

    Adding extra test

--- tests/testthat/test_create_dtm.R
+++ tests/testthat/test_create_dtm.R
@@ -56,3 +56,22 @@ test_that("creating a document term matrix with another book title", {
     }
   }
 })
+
+test_that("creating a document term matrix with book title, no stopwords", {
+  books <- data.frame(book = c("Book A", "Book B", "Book C"), 
+                      text = c("Once upon a time", "A long time ago", 
+                               "In a land far away"),
+                      stringsAsFactors=FALSE)
+  
+  if(require("tm", quietly = TRUE)) {
+    if(require("tidytext", quietly = TRUE)) {
+      dtm <- create_dtm(books, doc_title="book")
+      
+      # Expect 11 columns, 1 for id and 10 others representing words
+      expect_equal(length(dtm),11)
+      
+      # Expect 3 rows, one for each book
+      expect_equal(nrow(dtm),3)
+    }
+  }
+})
