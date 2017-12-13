53de9ac75bfb8a8f0d7da688e080f90572df1cf7
Caoilfhionn Lane <cl.datascience@gmail.com>
2017-12-13 12:04:43 +0000

    Add feature creation and fix bug in cbind

--- R/create_dtm.R
+++ R/create_dtm.R
@@ -6,7 +6,8 @@
 #' input to machine learning tasks
 #'
 #' @param corpus A data frame containing columns for title and text
-#' @param stopwords A data frame containing stop words
+#' @param filterwords A data frame containing words to filter on
+#' @param stop A boolean denoting whether to use filterwords as top words
 #' @param doc_title The column name containing document title
 #'
 #' @return A data frame of a document term matrix.
@@ -19,29 +20,37 @@
 #'                     "In a land far away"),
 #'                     stringsAsFactors=FALSE)
 #' book_dtm <- create_dtm(books)
-#' book_dtm_1 <- create_dtm(books,stopwords=stop_words)
+#' book_dtm_1 <- create_dtm(books,filterwords=stop_words)
 #' }
 #'
 #' @import dplyr
 #' @import tidytext
 #'
 #' @export
-create_dtm <- function(corpus, stopwords, doc_title="title"){
+create_dtm <- function(corpus, filterwords, 
+                       stop=TRUE, doc_title="title"){
   word <- text <- title <- dtm <- NULL
   
   corpus <- dplyr::rename_(corpus, title=doc_title)
   ngrams <- tidytext::unnest_tokens(corpus, word, text)
-  id <- corpus$title
   
-  if (!missing(stopwords)){
-    ngrams <- dplyr::filter(ngrams, !word %in% stopwords$word)
+  if (!missing(filterwords)){
+    if(stop==TRUE){
+      ngrams <- dplyr::filter(ngrams, !word %in% filterwords$word)
+    } else {
+      ngrams <- dplyr::filter(ngrams, word %in% filterwords$word)
+    }
   }
   
   ngram_counts <- dplyr::count(ngrams, title, word)
   dtm <- tidytext::cast_dtm(ngram_counts, title, word, n)
   data_dtm <- as.data.frame(as.matrix(dtm))
-  data_dtm <- cbind(id,data_dtm,row.names=NULL)
-  data_dtm$id <- as.character(data_dtm$id)
+  data_dtm <- cbind(title=rownames(data_dtm), data.frame(data_dtm, row.names=NULL))
+  data_dtm$title <- as.character(data_dtm$title)
+  corpus$title <- as.character(corpus$title)
+  data_dtm <- left_join(corpus,data_dtm,by="title")
+  data_dtm[is.na(data_dtm)] <- 0
+  data_dtm$title <- as.character(data_dtm$title)
   
   return(data_dtm)
 }
--- tests/testthat/test_create_dtm.R
+++ tests/testthat/test_create_dtm.R
@@ -11,15 +11,15 @@ test_that("creating a document term matrix", {
   if(require("tm", quietly = TRUE)) {
     dtm <- create_dtm(books)
     
-    # Expect 11 columns, 1 for id and 10 others representing words
-    expect_equal(length(dtm),11)
+    # Expect 12 columns, for id and words
+    expect_equal(length(dtm),12)
     
     # Expect 3 rows, one for each book
     expect_equal(nrow(dtm),3)
   }
 })
 
-test_that("creating a document term matrix with stopwords removed", {
+test_that("creating a document term matrix with filterwords removed", {
   
   books <- data.frame(title = c("Book A", "Book B", "Book C"), 
                       text = c("Once upon a time", "A long time ago", 
@@ -28,10 +28,31 @@ test_that("creating a document term matrix with stopwords removed", {
   
   if(require("tm", quietly = TRUE)) {
     if(require("tidytext", quietly = TRUE)) {
-      dtm <- create_dtm(books, stopwords=stop_words)
+      dtm <- create_dtm(books, filterwords=stop_words)
       
-      # Expect 11 columns, 1 for id and 3 others representing remaining words
-      expect_equal(length(dtm),4)
+      # Expect 5 columns, for id and words
+      expect_equal(length(dtm),5)
+      
+      # Expect 3 rows, one for each book
+      expect_equal(nrow(dtm),3)
+    }}
+})
+
+test_that("creating a document term matrix with selected features", {
+  
+  books <- data.frame(title = c("Book A", "Book B", "Book C"), 
+                      text = c("Once upon a time", "A long time ago", 
+                               "In a land far away"),
+                      stringsAsFactors=FALSE)
+  
+  features <- data.frame(word = c("once","far","away"))
+  
+  if(require("tm", quietly = TRUE)) {
+    if(require("tidytext", quietly = TRUE)) {
+      dtm <- create_dtm(books, filterwords=features, stop=FALSE)
+      
+      # Expect 5 columns, for id and words
+      expect_equal(length(dtm),5)
       
       # Expect 3 rows, one for each book
       expect_equal(nrow(dtm),3)
@@ -46,10 +67,10 @@ test_that("creating a document term matrix with another book title", {
   
   if(require("tm", quietly = TRUE)) {
     if(require("tidytext", quietly = TRUE)) {
-      dtm <- create_dtm(books, doc_title="book", stopwords=stop_words)
+      dtm <- create_dtm(books, doc_title="book", filterwords=stop_words)
       
-      # Expect 11 columns, 1 for id and 3 others representing remaining words
-      expect_equal(length(dtm),4)
+      # Expect 5 columns for id and words
+      expect_equal(length(dtm),5)
       
       # Expect 3 rows, one for each book
       expect_equal(nrow(dtm),3)
@@ -57,7 +78,7 @@ test_that("creating a document term matrix with another book title", {
   }
 })
 
-test_that("creating a document term matrix with book title, no stopwords", {
+test_that("creating a document term matrix with book title, no filterwords", {
   books <- data.frame(book = c("Book A", "Book B", "Book C"), 
                       text = c("Once upon a time", "A long time ago", 
                                "In a land far away"),
@@ -67,8 +88,8 @@ test_that("creating a document term matrix with book title, no stopwords", {
     if(require("tidytext", quietly = TRUE)) {
       dtm <- create_dtm(books, doc_title="book")
       
-      # Expect 11 columns, 1 for id and 10 others representing words
-      expect_equal(length(dtm),11)
+      # Expect 11 columns, for id and words
+      expect_equal(length(dtm),12)
       
       # Expect 3 rows, one for each book
       expect_equal(nrow(dtm),3)
